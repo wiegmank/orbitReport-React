7cc184a31e8e7abc56e51bf021e7901948c0c0fd
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-11-18 09:00:36 -0600

    assignment 6 complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import './styling.css'
+
+import satData from "./satData";
+
+export default function Banner() {
+    
+    return (
+        <>
+        <header>
+            Orbit Report
+        </header>
+        <p> Click on the buttons to see the satellites in that orbit type </p>
+        </>
+    )
+};
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,33 @@
+import './styling.css'
+//import satData from './satData';
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+
+     {sat.map((data, id) => {
+      return (
+
+     <tr key={id}>
+     <td>{data.name}</td>
+     <td>{data.type}</td>
+     <td>{data.launchDate}</td>
+     <td>{data.operational ? "Active" : "Inactive"}</td>
      </tr>
+  );
+})}
      </tbody>
    </table>
+
   );
 };
 
