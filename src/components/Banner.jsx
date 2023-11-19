3302e477751b381a061edeb3d15737b5aa4ea628
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-11-19 10:31:27 -0600

    fixed banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,9 @@ export default function Banner() {
     return (
         <>
         <header>
+            <h1>
             Orbit Report
+            </h1>
         </header>
         <p> Click on the buttons to see the satellites in that orbit type </p>
         </>
